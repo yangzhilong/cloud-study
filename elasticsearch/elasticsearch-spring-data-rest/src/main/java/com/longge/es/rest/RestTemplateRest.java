3c6ba56e30c43926d61fff20f034a8116d33a77a
roger yang <roger.yang@nike.com>
2019-12-06 17:08:04 +0800

    add get method

--- elasticsearch/elasticsearch-spring-data-rest/src/main/java/com/longge/es/rest/RestTemplateRest.java
+++ elasticsearch/elasticsearch-spring-data-rest/src/main/java/com/longge/es/rest/RestTemplateRest.java
@@ -6,8 +6,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.query.GetQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +32,11 @@ public class RestTemplateRest {
     @Autowired
     private ElasticsearchRestTemplate elasticsearchRestTemplate;
     
+    /**
+            *  添加
+     * @param dto
+     * @return
+     */
     @PostMapping("/add")
     public GlobalResponse<String> add(@RequestBody @Valid UserDto dto) {
         User user = BeanMapper.map(dto, User.class);
@@ -38,5 +46,15 @@ public class RestTemplateRest {
         IndexQuery query = new IndexQueryBuilder().withObject(user).build();
         String docId = elasticsearchRestTemplate.index(query);
         return GlobalResponse.buildSuccess(docId);
+    }
+    
+    @GetMapping("/get/{id}")
+    public GlobalResponse<UserDto> get(@PathVariable("id") String id) {
+    	GetQuery query = GetQuery.getById(id);
+    	User user = elasticsearchRestTemplate.queryForObject(query, User.class);
+    	if(null != user) {
+    		return GlobalResponse.buildSuccess(BeanMapper.map(user, UserDto.class));
+    	}
+    	return GlobalResponse.buildFail("404", "no data");
     }
 }
