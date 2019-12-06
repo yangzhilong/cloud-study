f82817aa4efc1bd7972e55e1eb325b046ed66052
roger yang <roger.yang@nike.com>
2019-12-06 17:34:07 +0800

    add api

--- elasticsearch/elasticsearch-spring-data-rest/src/main/java/com/longge/es/rest/RestTemplateRest.java
+++ elasticsearch/elasticsearch-spring-data-rest/src/main/java/com/longge/es/rest/RestTemplateRest.java
@@ -4,14 +4,18 @@ import java.util.Objects;
 
 import javax.validation.Valid;
 
+import org.elasticsearch.action.index.IndexRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.query.GetQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.UpdateQuery;
+import org.springframework.data.elasticsearch.core.query.UpdateQueryBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,11 +47,16 @@ public class RestTemplateRest {
         if(Objects.isNull(user.getId())) {
             user.setId(1L);
         }
-        IndexQuery query = new IndexQueryBuilder().withObject(user).build();
+        IndexQuery query = new IndexQueryBuilder().withId(String.valueOf(dto.getId())).withObject(user).build();
         String docId = elasticsearchRestTemplate.index(query);
         return GlobalResponse.buildSuccess(docId);
     }
     
+    /**
+            *   查询单条数据
+     * @param id
+     * @return
+     */
     @GetMapping("/get/{id}")
     public GlobalResponse<UserDto> get(@PathVariable("id") String id) {
     	GetQuery query = GetQuery.getById(id);
@@ -56,5 +65,23 @@ public class RestTemplateRest {
     		return GlobalResponse.buildSuccess(BeanMapper.map(user, UserDto.class));
     	}
     	return GlobalResponse.buildFail("404", "no data");
+    }
+    
+    /**
+            *  更新单条记录
+     * @param id
+     * @param dto
+     * @return
+     */
+    @PutMapping("/update/{id}")
+    public GlobalResponse<Boolean> update(@PathVariable("id") String id, @RequestBody @Valid UserDto dto) {
+    	User user = BeanMapper.map(dto, User.class);
+    	
+    	IndexRequest indexRequest = new IndexRequest();
+    	indexRequest.id(id);
+    	indexRequest.source(user);
+    	UpdateQuery query = new UpdateQueryBuilder().withId(id).withIndexRequest(indexRequest).build();
+    	elasticsearchRestTemplate.update(query);
+    	return GlobalResponse.buildSuccess(Boolean.TRUE);
     }
 }
