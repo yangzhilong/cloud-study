5b2e78f6ba1a3cc2d3130442f7fdbcda9f271b21
yangzhilong <yangzhilong@jshuii.com>
2019-12-08 20:48:34 +0800

    update es rest api demo

--- elasticsearch/elasticsearch-spring-data-rest/src/main/java/com/longge/es/domain/User.java
+++ elasticsearch/elasticsearch-spring-data-rest/src/main/java/com/longge/es/domain/User.java
@@ -8,12 +8,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
+ *  这里为什么要把type写成_doc呢，因为es的7版本已经将type废弃了，type统一为_doc，如果你要不写，springboot会默认取类的名字，也就是user作为type
  * @author roger yang
  * @date 12/06/2019
  */
 @Getter
 @Setter
-@Document(indexName = "user")
+@Document(indexName = "user", type="_doc")
 public class User implements Serializable {
     /**
      *
--- elasticsearch/elasticsearch-spring-data-rest/src/main/java/com/longge/es/rest/RestTemplateRest.java
+++ elasticsearch/elasticsearch-spring-data-rest/src/main/java/com/longge/es/rest/RestTemplateRest.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 import javax.validation.Valid;
 
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.query.GetQuery;
@@ -12,6 +13,7 @@ import org.springframework.data.elasticsearch.core.query.IndexQuery;
 import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.UpdateQuery;
 import org.springframework.data.elasticsearch.core.query.UpdateQueryBuilder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,10 +22,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
 import com.longge.common.dto.GlobalResponse;
 import com.longge.common.util.BeanMapper;
 import com.longge.es.domain.User;
 import com.longge.es.dto.UserDto;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 使用es 的rest api来操作ES
@@ -32,12 +37,13 @@ import com.longge.es.dto.UserDto;
  */
 @RestController
 @RequestMapping("/api/template")
+@Slf4j
 public class RestTemplateRest {
     @Autowired
     private ElasticsearchRestTemplate elasticsearchRestTemplate;
     
     /**
-            *  添加
+     *  添加
      * @param dto
      * @return
      */
@@ -53,7 +59,7 @@ public class RestTemplateRest {
     }
     
     /**
-            *   查询单条数据
+     *   查询单条数据
      * @param id
      * @return
      */
@@ -68,7 +74,7 @@ public class RestTemplateRest {
     }
     
     /**
-            *  更新单条记录
+     *  更新单条记录
      * @param id
      * @param dto
      * @return
@@ -76,12 +82,22 @@ public class RestTemplateRest {
     @PutMapping("/update/{id}")
     public GlobalResponse<Boolean> update(@PathVariable("id") String id, @RequestBody @Valid UserDto dto) {
     	User user = BeanMapper.map(dto, User.class);
-    	
     	IndexRequest indexRequest = new IndexRequest();
-    	indexRequest.id(id);
-    	indexRequest.source(user);
-    	UpdateQuery query = new UpdateQueryBuilder().withId(id).withIndexRequest(indexRequest).build();
+    	indexRequest.source(new Gson().toJson(user), XContentType.JSON);
+    	UpdateQuery query = new UpdateQueryBuilder().withClass(User.class).withId(id).withIndexRequest(indexRequest).build();
     	elasticsearchRestTemplate.update(query);
+    	return GlobalResponse.buildSuccess(Boolean.TRUE);
+    }
+    
+    /**
+     * 删除记录
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/delete/{id}")
+    public GlobalResponse<Boolean> delete(@PathVariable("id") String id) {
+    	String docId = this.elasticsearchRestTemplate.delete(User.class, id);
+    	log.info("delete user doc id is:{}", docId);
     	return GlobalResponse.buildSuccess(Boolean.TRUE);
     }
 }
